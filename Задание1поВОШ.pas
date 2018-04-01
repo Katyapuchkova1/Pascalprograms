c4853aba1a3f73c06fb14156006fe2d33bd9ae74
Katyapuchkova1 <Katyapuchkova1@gmail.com>
2018-04-01 12:29:04 +0300

    Solution of olimpiad
    Code of the first two solutions of the olimpiad

---
+++ Задание1поВОШ.pas
@@ -0,0 +1,21 @@
+﻿program EmmaStone;
+
+var A , B , C:integer;
+var N:real;
+
+begin
+
+writeln ('Введите три числа');
+
+readln (A , B , C);
+
+if A <= C then
+
+N := (C - A) / B else
+
+writeln ('Ошибка');
+
+writeln ('Максимальное значение лопастей ' , N);
+
+end.
+ 
---
+++ Задание2поВОШ.pas
@@ -0,0 +1,30 @@
+﻿program EmmaStone;
+
+var M , Ost , S :integer;
+var L , N:real;
+
+begin
+
+writeln ('Введите количество лопастей');
+readln (M);
+
+Ost := M mod 3;
+
+if Ost = 1 then 
+S := (M - 1 - (3 * 5));
+if Ost = 2 then
+S := (M - 2 - (3 * 2));
+
+N := (M - S) / 4;
+L := S / 3;
+
+writeln ('Количество спиннеров с 4-рьмя лопастями равно ' , N);
+writeln ('Количество спиннеров с 3-мя лопастями равно ' , L);
+
+end.
+
+
+
+
+
+
